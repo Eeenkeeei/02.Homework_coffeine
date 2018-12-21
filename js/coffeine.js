bdced33540f1d5e062c6a48564faa816f9fce084
student <romanilichev@mail.ru>
2018-12-22 02:13:30 +0300

    Fixed operator '=' (before '===') to '=='

--- js/coffeine.js
+++ js/coffeine.js
@@ -1,17 +1,17 @@
 export function returnMoney(drinkName, money) {
     let result=0;
     if (money<=100){
-        if (drinkName==='3v1'){
+        if (drinkName=='3v1'){
             return result=money-30;
 
         }
-        if (drinkName==='blackCoffee'){
+        if (drinkName=='blackCoffee'){
             return result=money-35;
         }
-        if (drinkName==='cream'){
+        if (drinkName=='cream'){
             return result=money-40;
         }
-        if (drinkName==='latte'){
+        if (drinkName=='latte'){
             return result=money-45;
         }
 
